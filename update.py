88abca9924b14f1eb955b216ac06df0729cd00dd
HYK <90774229+HYK-Nov@users.noreply.github.com>
2023-10-04 14:46:04 +0900

    Update update.py

--- update.py
+++ update.py
@@ -6,7 +6,10 @@ from urllib import parse
 HEADER="""# 
 # 백준 & 프로그래머스 문제 풀이 목록
 
-프로그래머스의 경우, 푼 문제 목록에 대한 마이그레이션이 필요합니다.
+제가 푼 백준과 프로그래머스 문제가 자동으로 커밋되는 곳입니다
+
+### boj
+[![Solved.ac프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj=yundori97)](https://solved.ac/yundori97)
 
 """
 
